f26841f059e262cf42bf001a134e6d7e23c8edf5
LasaleFamine <info@godev.space>
2017-09-22 12:17:11 +0200

    Fixed check on switch keys

--- src/lib/switchcase.js
+++ src/lib/switchcase.js
@@ -4,7 +4,7 @@ const _executeFunction = f =>
 	typeof f === 'function' ? f() : f;
 
 const _switch = cases => defaultCase => key =>
-	key in cases[key] ? cases[key] : defaultCase;
+	key in cases ? cases[key] : defaultCase;
 
 module.exports = cases => defaultCase => key =>
 	_executeFunction(_switch(cases)(defaultCase)(key));
